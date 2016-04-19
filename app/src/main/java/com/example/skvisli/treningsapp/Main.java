e58b8033efb69ec3d265630f3a8a849751c649d9
Thomas Wold <thomawol@stud.ntnu.no>
2016-04-19 10:31:07 +0200

    Add default activity

--- app/src/main/java/com/example/skvisli/treningsapp/Main.java
+++ app/src/main/java/com/example/skvisli/treningsapp/Main.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
