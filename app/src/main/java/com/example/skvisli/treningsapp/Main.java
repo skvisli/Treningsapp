24adb54ffbaaea7c5ef62f561336d380630b1222
Thomas Wold <thomawol@stud.ntnu.no>
2016-04-19 10:47:48 +0200

    Fixed everything

--- app/src/main/java/com/example/skvisli/treningsapp/Main.java
+++ app/src/main/java/com/example/skvisli/treningsapp/Main.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
