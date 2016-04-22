8a90d3016f8d0b02f2ac45049251029f677d1e66
Thomas Wold <thomawol@stud.ntnu.no>
2016-04-22 12:02:05 +0200

    added navigation on competitions and main

--- app/src/main/java/com/example/skvisli/treningsapp/Competitions.java
+++ app/src/main/java/com/example/skvisli/treningsapp/Competitions.java
@@ -1,16 +1,30 @@
 package com.example.skvisli.treningsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Competitions extends AppCompatActivity {
+
+    private ImageButton hjem;
+    private ImageButton skritt;
+    private ImageButton resultat;
+    private ImageButton profil;
+    private ImageButton instilling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_competitions);
+        hjem = (ImageButton) findViewById(R.id.hjem);
+        skritt = (ImageButton) findViewById(R.id.skritt);
+        resultat = (ImageButton) findViewById(R.id.resultat);
+        profil = (ImageButton) findViewById(R.id.profil);
+        instilling = (ImageButton) findViewById(R.id.instilling);
     }
 
     @Override
@@ -19,6 +33,26 @@ public class Competitions extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_competitions, menu);
         return true;
     }
+
+    public void goTo(View view){
+        if(hjem.isPressed()){
+            startActivity(new Intent(this, Main.class));
+        }
+        if(skritt.isPressed()){
+            startActivity(new Intent(this, Steps.class));
+        }
+        if(resultat.isPressed()){
+            startActivity(new Intent(this, Results.class));
+        }
+        if(profil.isPressed()){
+            startActivity(new Intent(this, Profile.class));
+        }
+        if(instilling.isPressed()){
+            startActivity(new Intent(this, Settings.class));
+        }
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
